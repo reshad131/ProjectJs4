b99f4e99abe01b824d920cd773045e1ea57e6048
reshad131 <161569107+reshad131@users.noreply.github.com>
2024-04-02 10:26:28 +0400

    First commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+// setInterval(() => {
+//     console.clear();
+//     const now = new Date();
+//     const saniye = now.getSeconds();
+//     const deqiqe = now.getMinutes();
+//     const saat = now.getHours();
+//     const gun = now.getDate();
+//     const ay = now.getMonth();
+//     const il = now.getFullYear();
+//     const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+//     console.log(`%c${ay}.${gun}.${il}  ${saat}:${deqiqe}:${saniye}` + " Azerbaijan,Baku", `color: ${randomColor}`);
+// }, 1000);
+
+
+// word=prompt("Enter a word")
+
+
+// for (let i = 0; i <= word.length * 2; i++) {
+//   const substringLength = i < word.length ? i : word.length * 2 - i;
+//   const substring = word.substring(0, substringLength);
+
+//   setTimeout(() => {
+//     console.log(substring);
+//   }, i * 500); 
+// }
